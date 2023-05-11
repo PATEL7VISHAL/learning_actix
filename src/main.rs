f2292d7deb031bf910ec169b4c30ecac0722551d
patel7vishal <pv251023@gmail.com>
2023-05-11 10:33:37 +0530

    getting started

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn root() -> impl Responder {
+    HttpResponse::Ok().body("Hello")
+}
+
+#[post("/greet")]
+async fn greet(name: String) -> impl Responder {
+    HttpResponse::Ok().body(format!("Hello {}", name))
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey There !")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(root)
+            .service(greet)
+            .route("/manual", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
